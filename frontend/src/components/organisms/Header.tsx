86d66282227ed5187aa69c8a2e2215af65510ddc
Senritsu420 <kubo3900@gmail.com>
2024-02-20 01:29:19 +0900

    Headerのロゴにトップへのリンクを付与

--- frontend/src/components/organisms/Header.tsx
+++ frontend/src/components/organisms/Header.tsx
@@ -1,20 +1,24 @@
 import { Flex, Image } from '@chakra-ui/react'
 import { SearchInput } from '../atoms/SearchInput'
 import { SearchIconComponent } from '../atoms/SearchIconComponent'
+import Link from 'next/link'
+import { CONST } from '@/const'
 
 export const Header = () => {
   return (
     <>
       <Flex bg="yellow.primary" w="100%" h="7vh" alignItems="center">
         <Flex justifyContent="space-between" h="80%" px="3%" w="100%">
-          <Image
-            src="/icons/techpulse_transparent_big_name.png"
-            alt="#"
-            w="auto"
-            h="100%"
-            objectFit="cover"
-            _hover={{ cursor: 'pointer' }}
-          />
+          <Link href={CONST.TOP}>
+            <Image
+              src="/icons/techpulse_transparent_big_name.png"
+              alt="#"
+              w="auto"
+              h="100%"
+              objectFit="cover"
+              _hover={{ cursor: 'pointer', opacity: '0.5' }}
+            />
+          </Link>
           <SearchIconComponent />
         </Flex>
       </Flex>
--- frontend/src/const.ts
+++ frontend/src/const.ts
@@ -1,5 +1,6 @@
 export const CONST = {
   API_BASE_PATH: "http://backend:8080",
+  TOP: "/",
   ARTICLES: "/articles",
   SEARCH_ARTICLES_IN_TITLE: "/articles/search?title="
 }
