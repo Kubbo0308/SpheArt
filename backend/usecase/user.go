b76561317dadfae4061e01dd26bd2cc601a7e3bc
Senritsu420 <kubo3900@gmail.com>
2024-02-21 23:56:29 +0900

    usecase追加

---
+++ backend/usecase/user.go
@@ -0,0 +1,60 @@
+package usecase
+
+import (
+	"backend/domain/model"
+	"backend/domain/repository"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserUsecase interface {
+	SignUp(user model.User) (model.UserResponse, error)
+	SignIn(user model.User) (string, error)
+}
+
+type userUsecase struct {
+	ur repository.UserRepository
+}
+
+func NewUserUsecase(ur repository.UserRepository) UserUsecase {
+	return &userUsecase{ur}
+}
+
+func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		return model.UserResponse{}, err
+	}
+	newUser := model.User{Email: user.Email, Password: string(hash)}
+	if err := uu.ur.CreateUser(&newUser); err != nil {
+		return model.UserResponse{}, err
+	}
+	resUser := model.UserResponse{
+		ID:    newUser.ID,
+		Email: newUser.Email,
+	}
+	return resUser, nil
+}
+
+func (uu *userUsecase) SignIn(user model.User) (string, error) {
+	checkUser := model.User{}
+	if err := uu.ur.UserByEmail(&checkUser, user.Email); err != nil {
+		return "", err
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(checkUser.Password), []byte(user.Password))
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+		"user_id": checkUser.ID,
+		"exp":     time.Now().Add(time.Hour * 3).Unix(),
+	})
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, err
+}
