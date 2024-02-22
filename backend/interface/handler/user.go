08530386fd76a6df04db57e9c8b92e6a75fbbb5a
Senritsu420 <kubo3900@gmail.com>
2024-02-23 08:44:19 +0900

    handler追加

---
+++ backend/interface/handler/user.go
@@ -0,0 +1,81 @@
+package handler
+
+import (
+	"backend/domain/model"
+	"backend/usecase"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type UserHandler interface {
+	SignUp(ctx echo.Context) error
+	SignIn(ctx echo.Context) error
+	SignOut(ctx echo.Context) error
+	CsrfToken(ctx echo.Context) error
+}
+
+type userHandler struct {
+	uu usecase.UserUsecase
+}
+
+func NewUserHandler(uu usecase.UserUsecase) UserHandler {
+	return &userHandler{uu}
+}
+
+func (uh *userHandler) SignUp(ctx echo.Context) error {
+	user := model.User{}
+	if err := ctx.Bind(&user); err != nil {
+		return ctx.JSON(http.StatusBadRequest, err.Error())
+	}
+	res, err := uh.uu.SignUp(user)
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return ctx.JSON(http.StatusCreated, res)
+}
+
+func (uh *userHandler) SignIn(ctx echo.Context) error {
+	user := model.User{}
+	if err := ctx.Bind(&user); err != nil {
+		return ctx.JSON(http.StatusBadRequest, err.Error())
+	}
+	tokenString, err := uh.uu.SignIn(user)
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, err.Error())
+	}
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = tokenString
+	cookie.Expires = time.Now().Add(3 * time.Hour)
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+	// cookie.Secure = true
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	ctx.SetCookie(cookie)
+	return ctx.NoContent(http.StatusOK)
+}
+
+func (uh *userHandler) SignOut(ctx echo.Context) error {
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = ""
+	cookie.Expires = time.Now()
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+	cookie.Secure = true
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	ctx.SetCookie(cookie)
+	return ctx.NoContent(http.StatusOK)
+}
+
+func (uh *userHandler) CsrfToken(ctx echo.Context) error {
+	token := ctx.Get("csrf").(string)
+	return ctx.JSON(http.StatusOK, echo.Map{
+		"csrf_token": token,
+	})
+}
