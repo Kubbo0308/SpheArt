4268d39fca6b6a68c58a95fe97ed7e428642a1b7
Senritsu420 <kubo3900@gmail.com>
2024-02-23 10:05:19 +0900

    認証系エンドポイント追加

--- backend/router/router.go
+++ backend/router/router.go
@@ -9,7 +9,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func NewRouter(ah handler.ArticleHandler) *echo.Echo {
+func NewRouter(ah handler.ArticleHandler, uh handler.UserHandler) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
@@ -31,5 +31,9 @@ func NewRouter(ah handler.ArticleHandler) *echo.Echo {
 
 	e.GET("/articles", ah.GetAllArticles)
 	e.GET("/articles/search", ah.SearchInArticleTitle)
+	e.POST("/signup", uh.SignUp)
+	e.POST("/signin", uh.SignIn)
+	e.POST("/signout", uh.SignOut)
+	e.GET("/csrf", uh.CsrfToken)
 	return e
 }
