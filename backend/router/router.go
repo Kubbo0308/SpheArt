83c7e04ec8b69d7930b6bbecc52d3b39cebed62e
Senritsu420 <kubo3900@gmail.com>
2024-02-23 14:58:49 +0900

    CORS追加

--- backend/router/router.go
+++ backend/router/router.go
@@ -2,12 +2,23 @@ package router
 
 import (
 	"backend/interface/handler"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter(ah handler.ArticleHandler, uh handler.UserHandler) *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
+		AllowHeaders: []string{
+			echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
+			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken,
+		},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
+		AllowCredentials: true,
+	}))
 
 	e.GET("/articles", ah.GetAllArticles)
 	e.GET("/articles/search", ah.SearchInArticleTitle)
