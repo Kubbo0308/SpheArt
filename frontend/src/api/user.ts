a9fddb47bb1f9c8709282cfafc3fb16f4cc3c9a8
Senritsu420 <kubo3900@gmail.com>
2024-02-24 16:08:34 +0900

    フロントエンドのAPIを定義

---
+++ frontend/src/api/user.ts
@@ -0,0 +1,67 @@
+import { CONST } from "@/const"
+import { fetchCsrfToken } from "./csrf"
+
+interface UserInfo {
+  email: string
+  password: string
+}
+
+export async function SignUp(props: UserInfo) {
+  const {email, password} = props
+  const csrfToken = await fetchCsrfToken()
+  const res = await fetch(`${CONST.API_BASE_PATH}${CONST.SIGN_UP}`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': csrfToken, // ヘッダーにCSRFトークンをセット
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password
+    }),
+  })
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json
+}
+
+export async function SignIn(props: UserInfo) {
+  const {email, password} = props
+  const csrfToken = await fetchCsrfToken()
+  const res = await fetch(`${CONST.API_BASE_PATH}${CONST.SIGN_IN}`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': csrfToken, // ヘッダーにCSRFトークンをセット
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password
+    }),
+  })
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return
+}
+
+export async function SignOut(props: UserInfo) {
+  const {email, password} = props
+  const csrfToken = await fetchCsrfToken()
+  const res = await fetch(`${CONST.API_BASE_PATH}${CONST.SIGN_OUT}`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': csrfToken, // ヘッダーにCSRFトークンをセット
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password
+    }),
+  })
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return
+}
--- frontend/src/const.ts
+++ frontend/src/const.ts
@@ -1,6 +1,10 @@
 export const CONST = {
   API_BASE_PATH: "http://backend:8080",
   TOP: "/",
+  CSRF_TOKEN: "/csrf",
   ARTICLES: "/articles",
-  SEARCH_ARTICLES_IN_TITLE: "/articles/search?title="
+  SEARCH_ARTICLES_IN_TITLE: "/articles/search?title=",
+  SIGN_UP: "/signup",
+  SIGN_IN: "/signin",
+  SIGN_OUT: "/signout"
 }
