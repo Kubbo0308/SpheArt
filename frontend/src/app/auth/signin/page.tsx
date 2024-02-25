052021980334288c1faeef5f35540b3e7376f114
Senritsu420 <kubo3900@gmail.com>
2024-02-25 17:53:47 +0900

    サインアップページへの遷移追加

--- frontend/src/app/auth/signin/page.tsx
+++ frontend/src/app/auth/signin/page.tsx
@@ -1,3 +1,6 @@
-export default async function SignIn() {
-  return <h1>signin</h1>
+import { CONST } from '@/const'
+import { Link } from '@chakra-ui/react'
+
+export default function SignIn() {
+  return <Link href={`${CONST.AUTH}${CONST.SIGN_UP}`}>新規登録はこちら</Link>
 }
--- frontend/src/app/provider.tsx
+++ frontend/src/app/provider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { RecoilRoot } from 'recoil'
-import { theme } from '../../theme/theme'
+import { theme } from '../theme/theme'
 import { ChakraProvider } from '@chakra-ui/provider'
 
 export function Provider({ children }: { children: React.ReactNode }) {
