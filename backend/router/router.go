d8d5e4bb2b7fba698aa394dad9db4b6c5e1e8845
Senritsu420 <85555822+Senritsu420@users.noreply.github.com>
2024-03-03 19:14:48 +0900

    Revert "CSRFトークンでの認証追加"

--- backend/router/router.go
+++ backend/router/router.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"backend/interface/handler"
-	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
@@ -20,21 +19,11 @@ func NewRouter(ah handler.ArticleHandler, uh handler.UserHandler) *echo.Echo {
 		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
 		AllowCredentials: true,
 	}))
-	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
-		CookiePath:     "/",
-		CookieDomain:   os.Getenv("API_DOMAIN"),
-		CookieHTTPOnly: true,
-		// CookieSameSite: http.SameSiteNoneMode,
-		// TODO: POST MANでの動作確認用なので、公開時にNoneModeに変更する
-		CookieSameSite: http.SameSiteDefaultMode,
-		// CookieMaxAge: 60,
-	}))
 
 	e.GET("/articles", ah.GetAllArticles)
 	e.GET("/articles/search", ah.SearchInArticleTitle)
 	e.POST("/signup", uh.SignUp)
 	e.POST("/signin", uh.SignIn)
 	e.POST("/signout", uh.SignOut)
-	e.GET("/csrf", uh.CsrfToken)
 	return e
 }
