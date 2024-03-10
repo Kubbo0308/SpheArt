4dcccf8413f18794b7ca49a77c150fb75b765974
Senritsu420 <kubo3900@gmail.com>
2024-03-10 16:56:59 +0900

    persistence追加

--- backend/infrastrcuture/persistence/bookmark.go
+++ backend/infrastrcuture/persistence/bookmark.go
@@ -16,9 +16,21 @@ func NewBookmarkPersistence(db *gorm.DB) repository.BookmarkRepository {
 }
 
 func (bp *bookmarkPersistence) AllBookmarkByUserId(userId uint) ([]model.Bookmark, error) {
-	return []model.Bookmark{}, nil
+	bookmarks := []model.Bookmark{}
+	res := bp.db.Where("user_id = ?", userId).Find(&bookmarks)
+	if res.Error != nil {
+		return []model.Bookmark{}, res.Error
+	}
+	return bookmarks, nil
 }
 
 func (bp *bookmarkPersistence) CreateBookmark(userId uint, articleId uint) error {
+	bookmark := model.Bookmark{
+		UserID:    userId,
+		ArticleID: articleId,
+	}
+	if err := bp.db.Create(&bookmark).Error; err != nil {
+		return err
+	}
 	return nil
 }
