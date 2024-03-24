a4c6794ad621d6ad4c65b3ceaf5810e75b9c20ff
Senritsu420 <kubo3900@gmail.com>
2024-03-24 18:44:48 +0900

    Client、Serverで使えるようにAPIファイルを分割

--- frontend/src/api/postBookmark.ts
+++ frontend/src/api/postBookmark.ts
@@ -1,17 +1,5 @@
 import { CONST } from "@/const"
 
-export async function GetAllBookmark() {
-  const res = await fetch(`http://localhost:8080${CONST.BOOKMARK}`, {
-    method: "GET",
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    credentials: "include", // Cookieを含める
-  })
-  const data = await res.json()
-  return { status: res.status, data: data }
-}
-
 export async function PostBookmark(articleId: string) {
   const res = await fetch(`http://localhost:8080${CONST.BOOKMARK}/${articleId}`, {
     method: "POST",
