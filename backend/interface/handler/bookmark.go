4898ad3a7ff6b4e219a033ec7e67b2a7dc7afb9b
Senritsu420 <kubo3900@gmail.com>
2024-03-26 22:20:14 +0900

    interface追加

--- backend/interface/handler/bookmark.go
+++ backend/interface/handler/bookmark.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"backend/usecase"
 	"net/http"
+	"strconv"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
@@ -10,6 +11,7 @@ import (
 
 type BookmarkHandler interface {
 	AllBookmark(ctx echo.Context) error
+	BookmarkPerPage(ctx echo.Context) error
 	PostBookmark(ctx echo.Context) error
 }
 
@@ -26,7 +28,27 @@ func (bh *bookmarkHandler) AllBookmark(ctx echo.Context) error {
 	claims := user.Claims.(jwt.MapClaims)
 	userId := claims["user_id"]
 
-	res, err := bh.bu.BookmarkedArticle(uint(userId.(float64)))
+	res, err := bh.bu.AllBookmarkedArticle(uint(userId.(float64)))
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return ctx.JSON(http.StatusOK, res)
+}
+
+func (bh *bookmarkHandler) BookmarkPerPage(ctx echo.Context) error {
+	user := ctx.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+
+	queryParam := ctx.QueryParam("per_page")
+	perPage, err := strconv.Atoi(queryParam)
+	if err != nil {
+		// 変換に失敗した場合のエラーハンドリング
+		// 例えば、デフォルト値を設定する、エラーレスポンスを返すなど
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid per_page parameter"})
+	}
+
+	res, err := bh.bu.BookmarkedArticlePerPage(uint(userId.(float64)), perPage)
 	if err != nil {
 		return ctx.JSON(http.StatusInternalServerError, err.Error())
 	}
