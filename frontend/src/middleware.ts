a217226885fdfbf579dd2b6816a39eb568a26406
Senritsu420 <kubo3900@gmail.com>
2024-04-03 07:06:45 +0900

    リダイレクト追加

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,10 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
+import { CONST } from "./const";
 
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.get('token')
   if (cookie === undefined) {
-    if (request.nextUrl.pathname === "/bookmark") {
-      return NextResponse.redirect(new URL("/", request.url))
+    if (request.nextUrl.pathname === CONST.BOOKMARK) {
+      return NextResponse.redirect(new URL(`${CONST.AUTH}${CONST.SIGN_IN}`, request.url))
+    }
+
+    if (request.nextUrl.pathname === `${CONST.AUTH}${CONST.SIGN_OUT}`) {
+      return NextResponse.redirect(new URL(CONST.TOP, request.url))
+    }
+  } else {
+    if (request.nextUrl.pathname === `${CONST.AUTH}${CONST.SIGN_IN}`) {
+      return NextResponse.redirect(new URL(CONST.TOP, request.url))
     }
   }
 }
