6be9a44e695b3cc8190c711fc49dc4eceb728c1f
Senritsu420 <kubo3900@gmail.com>
2024-04-06 18:29:01 +0900

    サインインページのデザイン修正

--- frontend/src/app/auth/signin/page.tsx
+++ frontend/src/app/auth/signin/page.tsx
@@ -1,12 +1,5 @@
 import { SignInPage } from '@/components/pages/Auth/SignIn/SignInPage'
-import { CONST } from '@/const'
-import { Link } from '@chakra-ui/react'
 
 export default function SignIn() {
-  return (
-    <>
-      <SignInPage />
-      <Link href={`${CONST.AUTH}${CONST.SIGN_UP}`}>新規登録はこちら</Link>
-    </>
-  )
+  return <SignInPage />
 }
