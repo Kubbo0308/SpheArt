a67ac62ea59212901e7995a1a4d39cb14ef8ae95
Senritsu420 <kubo3900@gmail.com>
2024-04-07 14:48:51 +0900

    記事のUI作成

--- frontend/src/components/atoms/ArticleListItem.tsx
+++ frontend/src/components/atoms/ArticleListItem.tsx
@@ -6,6 +6,7 @@ import { AttachmentIcon } from '@chakra-ui/icons'
 import { ListItem, Text, Box, Flex, Spacer, Image, Badge, Link, Button } from '@chakra-ui/react'
 import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies'
 import { useState } from 'react'
+import { BookmarkButton } from './BookmarkButton'
 
 export type ArticleProps = {
   id: number
@@ -41,23 +42,6 @@ export const ArticleListItem = (props: ArticleListItemProps) => {
     }
   }
 
-  // SVGのいいねアイコン
-  const LikeIcon = () => (
-    <svg
-      viewBox="0 0 24 24"
-      width="16"
-      height="16"
-      stroke="currentColor"
-      strokeWidth="2"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      color="red.500"
-    >
-      <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-    </svg>
-  )
-
   // 日付のフォーマット
   const formatDate = (dateString: string) => {
     const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' }
@@ -65,44 +49,37 @@ export const ArticleListItem = (props: ArticleListItemProps) => {
   }
 
   return (
-    <ListItem p={4} borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="sm" bg="white.primary">
-      <Flex alignItems="center">
-        <Image
-          borderRadius="full"
-          boxSize="50px"
-          src={article.publisher_image_url}
-          alt={article.publisher_name}
-          mr={4}
-        />
-        <Box flex="1">
-          <Text fontWeight="bold" fontSize="lg">
-            <Link href={article.url} isExternal color="teal.500">
-              {article.title}
-            </Link>
+    <ListItem borderRadius="8px" overflow="hidden" boxShadow="sm" bg="white.primary" w="320px">
+      <Image
+        // src={article.publisher_image_url}
+        src="/no_image.svg"
+        alt={article.publisher_name}
+        h="180px"
+      />
+      <Box p="10px">
+        <Text fontSize="16px" fontWeight={700} lineHeight={1.8}>
+          <Link href={article.url} isExternal color="teal.500">
+            {article.title}
+          </Link>
+        </Text>
+        {/* <Text color="gray.500" fontSize="sm">
+          Published by {article.publisher_name} on {formatDate(article.created_at)}
+        </Text> */}
+        <Flex mt="10px" justifyContent="space-between" alignItems="center">
+          <Text fontSize="16px" fontWeight={500}>
+            {formatDate(article.created_at)}
           </Text>
-          <Text color="gray.500" fontSize="sm">
-            Published by {article.publisher_name} on {formatDate(article.created_at)}
-          </Text>
-        </Box>
-        <Spacer />
-        <Box textAlign="right">
-          <Badge colorScheme="purple" mr={2}>
-            {article.quote_source}
-          </Badge>
-          <Button variant="ghost" colorScheme="red" size="sm" leftIcon={<LikeIcon />}>
-            {article.likes_count}
-          </Button>
-          {isLogin && (
-            <AttachmentIcon
-              _hover={{ cursor: 'pointer' }}
-              color={isBookmark ? 'yellow.primary' : 'gray.placeholder'}
-              onClick={() => postBookmark(String(article.id))}
-            >
-              ブックマーク
-            </AttachmentIcon>
-          )}
-        </Box>
-      </Flex>
+          <Flex gap="10px" alignItems="center">
+            <Flex gap="3px" alignItems="center">
+              <Image src="/heart_256.svg" alt="" w="16px" h="16px" />
+              <Text fontSize="16px" fontWeight={500}>
+                {article.likes_count}
+              </Text>
+            </Flex>
+            <BookmarkButton onClick={() => postBookmark(String(article.id))} isBookmark={isBookmark} />
+          </Flex>
+        </Flex>
+      </Box>
     </ListItem>
   )
 }
