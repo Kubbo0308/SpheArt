1536d12dd0de27552640003d952edcc40edf6e4b
Senritsu420 <kubo3900@gmail.com>
2024-04-13 23:27:34 +0900

    OGP画像取得処理をutilsとして分割

--- backend/batch/qiita.go
+++ backend/batch/qiita.go
@@ -2,15 +2,13 @@ package batch
 
 import (
 	"backend/domain/model"
+	"backend/utils"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"strings"
-	"time"
 
-	"github.com/dyatlov/go-opengraph/opengraph"
 	"gorm.io/gorm"
 )
 
@@ -64,53 +62,16 @@ func GetQiitaArticleFromAPI(jsonData *[]model.QiitaResponse) error {
 	return nil
 }
 
-// URLからOGPのインスタンスを取得する
-func GetOGPImageFromURL(url string) (string, error) {
-	// http.Client の設定
-	client := &http.Client{
-		// 10秒のタイムアウト
-		Timeout: 10 * time.Second,
-	}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return "", err
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-
-	// ライブラリのOGPインスタンス
-	og := opengraph.NewOpenGraph()
-	// HTMLの解析
-	err = og.ProcessHTML(strings.NewReader(string(body)))
-	if err != nil {
-		return "", err
-	}
-
-	// OGPの画像URLを取得
-	if len(og.Images) > 0 {
-		return og.Images[0].URL, nil
-	}
-	// 画像が見つからない場合は空文字を返す
-	return "", nil
-
-}
-
 // 型を変換する
 func ConvertQiitaResponsesToArticles(qiitaResponses []model.QiitaResponse) []model.Article {
 	var articles []model.Article
 	for _, qiitaResp := range qiitaResponses {
 		// URLからOGP画像のURL取得
-		ogpImageUrl, err := GetOGPImageFromURL(qiitaResp.Url)
+		ogpImageUrl, err := utils.GetOGPImageFromURL(qiitaResp.Url)
 		if err != nil {
 			ogpImageUrl = ""
 		}
+
 		articles = append(articles, model.Article{
 			ID:                qiitaResp.Id,
 			Title:             qiitaResp.Title,
