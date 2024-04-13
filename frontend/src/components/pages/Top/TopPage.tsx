52a9b144964fd66abdf0dfe62805ae362b73581c
Senritsu420 <kubo3900@gmail.com>
2024-04-13 13:55:54 +0900

    トップページに無限スクロール実装

--- frontend/src/components/pages/Top/TopPage.tsx
+++ frontend/src/components/pages/Top/TopPage.tsx
@@ -1,10 +1,9 @@
 'use client'
 
 import { ArticleList } from '@/components/organisms/ArticleList'
-import { Button, Container, Flex } from '@chakra-ui/react'
+import { Box, Container } from '@chakra-ui/react'
 import { useTopPageHooks } from './TopPage.hooks'
 import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies'
-import { Suspense } from 'react'
 import { Loading } from '@/components/molecules/Loading'
 
 interface TopPageProps {
@@ -13,16 +12,12 @@ interface TopPageProps {
 
 export const TopPage = (props: TopPageProps) => {
   const { token } = props
-  const { articles, goNextPage, backPreviousPage } = useTopPageHooks()
+  const { articles, loader } = useTopPageHooks()
   return (
     <Container maxW="container.md" py="5%">
-      <Suspense fallback={<Loading />}>
-        <ArticleList articles={articles} token={token} />
-      </Suspense>
-      <Flex gap="20px">
-        <Button onClick={backPreviousPage}>前へ</Button>
-        <Button onClick={goNextPage}>次へ</Button>
-      </Flex>
+      <ArticleList articles={articles} token={token} />
+      <Box ref={loader} h="1px" mt="19px" />
+      <Loading />
     </Container>
   )
 }
