d21f5cd9336d738090ad343df4828cf9504b41b2
Senritsu420 <kubo3900@gmail.com>
2024-04-13 23:19:59 +0900

    データベース再定義

--- backend/domain/model/bookmark.go
+++ backend/domain/model/bookmark.go
@@ -2,6 +2,6 @@ package model
 
 type Bookmark struct {
 	ID        uint   `json:"id" gorm:"primaryKey"`
-	UserID    uint   `json:"user_id"`
-	ArticleID string `json:"article_id"`
+	UserID    uint   `json:"user_id" gorm:"index"`
+	ArticleID string `json:"article_id" gorm:"type:varchar(191);index"` // VARCHARに変更
 }
