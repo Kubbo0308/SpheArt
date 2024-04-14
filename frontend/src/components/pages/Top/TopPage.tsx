a5dc43db6ec03eb2455a3337e2e2dcddaca4585c
Senritsu420 <kubo3900@gmail.com>
2024-04-14 17:21:02 +0900

    仮でページごとのタイトル配置

--- frontend/src/components/pages/Top/TopPage.tsx
+++ frontend/src/components/pages/Top/TopPage.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ArticleList } from '@/components/organisms/ArticleList'
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Text } from '@chakra-ui/react'
 import { useTopPageHooks } from './TopPage.hooks'
 import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies'
 import { Loading } from '@/components/molecules/Loading'
@@ -14,10 +14,15 @@ export const TopPage = (props: TopPageProps) => {
   const { token } = props
   const { articles, loader, isVisible } = useTopPageHooks()
   return (
-    <Container maxW="container.md" py="5%">
-      <ArticleList articles={articles} token={token} />
-      <Box ref={loader} h="1px" mt="19px" />
-      {isVisible && <Loading />}
-    </Container>
+    <>
+      <Text fontSize="32px" fontWeight={600} lineHeight={1.8} mt="30px" textAlign="center">
+        新着記事一覧
+      </Text>
+      <Container maxW="container.md" py="5%">
+        <ArticleList articles={articles} token={token} />
+        <Box ref={loader} h="1px" mt="19px" />
+        {isVisible && <Loading />}
+      </Container>
+    </>
   )
 }
