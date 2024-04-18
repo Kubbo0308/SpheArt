545f2370b892844a2767510062c6bc46163b9391
Senritsu420 <kubo3900@gmail.com>
2024-04-19 01:54:15 +0900

    diパス変更

--- backend/interface/handler/http/article.go
+++ backend/interface/handler/http/article.go
@@ -7,19 +7,50 @@ import (
 	"strconv"
 )
 
-type ArticleSearchHandler interface {
+type ArticleHandler interface {
+	ArticlesPerPage(w http.ResponseWriter, r *http.Request)
+	AllArticles(w http.ResponseWriter, r *http.Request)
 	SearchInArticleTitle(w http.ResponseWriter, r *http.Request)
 }
 
-type articleSearchHandler struct {
+type articleHandler struct {
 	au usecase.ArticleUsecase
 }
 
-func NewArticleSearchHandler(au usecase.ArticleUsecase) ArticleSearchHandler {
-	return &articleSearchHandler{au}
+func NewArticleHandler(au usecase.ArticleUsecase) ArticleHandler {
+	return &articleHandler{au}
 }
 
-func (ah *articleSearchHandler) SearchInArticleTitle(w http.ResponseWriter, r *http.Request) {
+func (ah *articleHandler) ArticlesPerPage(w http.ResponseWriter, r *http.Request) {
+	queryParam := r.URL.Query().Get("per_page")
+	perPage, err := strconv.Atoi(queryParam)
+	if err != nil {
+		http.Error(w, "Invalid per_page parameter", http.StatusBadRequest)
+		return
+	}
+
+	res, err := ah.au.ArticlesPerPage(perPage)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+func (ah *articleHandler) AllArticles(w http.ResponseWriter, r *http.Request) {
+	res, err := ah.au.AllArticles()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+func (ah *articleHandler) SearchInArticleTitle(w http.ResponseWriter, r *http.Request) {
 	queryParamTitle := r.URL.Query().Get("title")
 	searchTitle := "%" + queryParamTitle + "%"
 
