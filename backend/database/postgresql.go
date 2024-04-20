fd110886055d29daebbadd46bc8e6ab77f9c5267
Senritsu420 <kubo3900@gmail.com>
2024-04-20 17:21:32 +0900

    環境変数名変更

--- backend/database/postgresql.go
+++ backend/database/postgresql.go
@@ -13,7 +13,7 @@ func NewPostgreSQLDB() *gorm.DB {
 	// MySQLに接続
 	dsn := fmt.Sprintf(`host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai`,
 		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), os.Getenv("POSTGRES_PORT"))
+		os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DATABASE"), os.Getenv("POSTGRES_PORT"))
 	if err := RetryConnectDB(postgres.Open(dsn), &gorm.Config{}, 100); err != nil {
 		log.Fatalln(err)
 	}
