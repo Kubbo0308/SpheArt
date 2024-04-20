96741c3c266ac09bcd816ebcc4b6ee3ef6965092
Senritsu420 <kubo3900@gmail.com>
2024-04-20 23:02:45 +0900

    環境によって使用するDSNを変更

--- backend/database/postgresql.go
+++ backend/database/postgresql.go
@@ -10,12 +10,18 @@ import (
 )
 
 func NewPostgreSQLDB() *gorm.DB {
-	// MySQLに接続
+	// PostgreSQLに接続
 	dsn := fmt.Sprintf(`host=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Shanghai`,
 		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DATABASE"))
-	if err := RetryConnectDB(postgres.Open(dsn), &gorm.Config{}, 100); err != nil {
-		log.Fatalln(err)
+	if os.Getenv("GO_ENV") == "dev" {
+		if err := RetryConnectDB(postgres.Open(dsn), &gorm.Config{}, 100); err != nil {
+			log.Fatalln(err)
+		}
+	} else if os.Getenv("GO_ENV") == "prod" {
+		if err := RetryConnectDB(postgres.Open(os.Getenv("POSTGRES_URL")), &gorm.Config{}, 100); err != nil {
+			log.Fatalln(err)
+		}
 	}
 
 	fmt.Println("Connected")
