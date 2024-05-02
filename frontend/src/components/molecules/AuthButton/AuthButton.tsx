25f78b371c234148d557f174f1a6cc46bb843fa5
Senritsu420 <kubo3900@gmail.com>
2024-05-03 00:11:04 +0900

    サインアウトをRoute Handlerで実装

--- frontend/src/components/molecules/AuthButton/AuthButton.tsx
+++ frontend/src/components/molecules/AuthButton/AuthButton.tsx
@@ -3,8 +3,6 @@
 import { useAuthButton } from './AuthButton.hooks'
 import { LinkButton } from '@/components/atoms/LinkButton'
 import { CONST } from '@/const'
-import { Button, Flex, Image, Menu, MenuButton, MenuDivider, MenuItem, MenuList } from '@chakra-ui/react'
-import Link from 'next/link'
 import { HeaderMenu } from '../HeaderMenu'
 
 export const AuthButton = () => {
