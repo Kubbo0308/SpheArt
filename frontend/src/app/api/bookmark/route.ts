30c225c3418931a2b9721d9175b56e8371c49182
Senritsu420 <kubo3900@gmail.com>
2024-05-03 00:58:08 +0900

    ブックマークをRoute Handlerで実装

---
+++ frontend/src/app/api/bookmark/route.ts
@@ -0,0 +1,68 @@
+import { CONST } from "@/const"
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const per_page = searchParams.get('per_page')
+  // クライアントからのリクエストヘッダーからCookieを取得
+  const clientCookies = request.headers.get('cookie');
+  // headersオブジェクトを定義し、Cookieがnullでない場合のみCookieヘッダーを設定
+  const headers: HeadersInit = {
+    'Content-Type': 'application/json'
+  };
+  if (clientCookies) {
+    headers.Cookie = clientCookies; // Cookieがnullでなければヘッダーに追加
+  }
+
+  const res = await fetch(`${process.env.API_URL}/api${CONST.BOOKMARK}?per_page=${per_page}`,
+  {
+    method: "GET",
+    headers: headers,
+    cache: 'no-cache',
+    credentials: "include", // Cookieを含める
+  })
+  const data = await res.json()
+
+  // Responseオブジェクトを生成してステータスコードとデータを返します
+  return new Response(JSON.stringify({
+    status: res.status,
+    data: data
+  }), {
+    status: 200,  // 成功した処理のHTTPステータスコード
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
+
+export async function POST(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const articleId = searchParams.get('articleId')
+  // クライアントからのリクエストヘッダーからCookieを取得
+  const clientCookies = request.headers.get('cookie');
+  // headersオブジェクトを定義し、Cookieがnullでない場合のみCookieヘッダーを設定
+  const headers: HeadersInit = {
+    'Content-Type': 'application/json'
+  };
+  if (clientCookies) {
+    headers.Cookie = clientCookies; // Cookieがnullでなければヘッダーに追加
+  }
+
+  const res = await fetch(`${process.env.API_URL}/api${CONST.BOOKMARK}/${articleId}`,
+  {
+    method: "POST",
+    headers: headers,
+    credentials: "include", // Cookieを含める
+  })
+  const data = await res.json()
+
+  // Responseオブジェクトを生成してステータスコードとデータを返します
+  return new Response(JSON.stringify({
+    status: res.status,
+    data: data
+  }), {
+    status: 200,  // 成功した処理のHTTPステータスコード
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
